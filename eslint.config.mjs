09299507d3bbe4b81997c0bd05e88f185d644d81
Raja Krishna <rajakrishna214@gmail.com>
2025-07-18 13:58:59 -0500

    chore: Add eslint rules and shadcn components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import pluginNext from '@next/eslint-plugin-next';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+const eslintConfig = [...compat.config({
+  extends: ['next/core-web-vitals', 'next/typescript','prettier','next'],
+}),{
+  rules: {
+    'no-console': ['warn'],
+  },
+  plugins: {
+    '@next/next': pluginNext,
+  },
+}];
 
 export default eslintConfig;
