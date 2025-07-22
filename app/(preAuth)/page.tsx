e1491af867e6517fb8c9807ce01048304674170b
Diego Espino <actual.mensch0r@icloud.com>
2025-07-22 11:28:04 -0500

    feat: Add Desktop Layout (Gym-22)

---
+++ app/(preAuth)/page.tsx
@@ -0,0 +1,7 @@
+export default function AuthLandingPage() {
+    return (
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+            <h1 className="text-4xl font-bold text-center">Hello World</h1>
+        </div>
+    );
+}
