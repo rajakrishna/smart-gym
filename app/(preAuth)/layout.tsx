04deb338b0cda45e8a72dd387a43fe3ad757705b
Diego (D) <actual.mensch0r@icloud.com>
2025-07-22 16:07:19 -0500

    Feat: Update layout.tsx to add Next metadata and proper tags

--- app/(preAuth)/layout.tsx
+++ app/(preAuth)/layout.tsx
@@ -1,18 +1,42 @@
 import { AuthNavbar } from '@/components/layouts/auth/navbar';
 import { AuthFooter } from '@/components/layouts/auth/footer';
 
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import './../globals.css';
+
+const geistSans = Geist({
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+    title: 'Title',
+    description: 'Description',
+};
+
 export default function AuthLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <div className="min-h-screen flex flex-col">
-            <AuthNavbar />
-            <main className="flex-1">
-                {children}
-            </main>
-            <AuthFooter />
-        </div>
+        <html lang='en'>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <div className="min-h-screen flex flex-col">
+                    <AuthNavbar />
+                    <main className="flex-1">
+                        {children}
+                    </main>
+                    <AuthFooter />
+                </div>
+            </body>
+        </html>
     );
 }
