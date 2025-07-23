5d1b2af15777878253180e61e6524717acdff9a4
noahwhlim <noahhlimwh@gmail.com>
2025-07-23 02:43:26 -0500

    fixed table name

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,11 +4,13 @@ import { createClient } from '@/utils/supabase/server';
 export async function GET() {
   const supabase = await createClient();
 
-  const { data: users, error } = await supabase.from('users').select();
+  const { data: users, error } = await supabase.from('User').select();
 
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 
   return NextResponse.json(users);
+  // console.log(users)
+  // return "Hello world"
 }
