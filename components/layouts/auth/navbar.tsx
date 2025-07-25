5adfec710d50b498183b3ffa0ed624d96c94eeff
Joe Aguado <aguado.joe@gmail.com>
2025-07-25 12:17:40 -0500

    feat: add demo video and style updates to navbar (GYM-65)

--- components/layouts/auth/navbar.tsx
+++ components/layouts/auth/navbar.tsx
@@ -7,19 +7,17 @@ import LABELS from '@/constants/labels';
 
 export function AuthNavbar() {
     return (
-        <nav className="border-b bg-background/95 backdrop-blur container mx-auto px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
+        <nav className="absolute top-8 left-0 w-full z-50 px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between bg-transparent text-white">
 
             {/* Logo */}
-            <Link href="/" className="flex items-center space-x-2">
-                <ICONS.dumbbell className="h-6 w-6" />
-                <span className="font-bold text-xl">{LABELS.app.name}</span>
+            <Link href="/" className="">
+                <p className="font-bold text-[58px] uppercase">{LABELS.app.name}</p>
             </Link>
 
             {/* Right hand side of navbar */}
-            <Button variant="ghost" asChild>
+            <Button asChild className='w-[100px] text-black' >
                 {/* TODO: Change this to scroll to the login section on the landing page */}
                 <Link href="/login">
-                    <ICONS.login className="mr-2 h-4 w-4" />
                     {LABELS.nav.auth.login}
                 </Link>
             </Button>
