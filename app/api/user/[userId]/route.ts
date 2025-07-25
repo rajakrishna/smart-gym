e355368408d1cc47c85ae824efbf81d76833bc9a
noahwhlim <noahhlimwh@gmail.com>
2025-07-25 15:53:54 -0500

    added endpoint to get single user

---
+++ app/api/user/[userId]/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+
+import { createClient } from '@/utils/supabase/server';
+
+export async function GET(request: Request, { params }: { params: { userId: string } }) {
+  const { userId } = params;
+
+  const supabase = await createClient();
+
+  const { data: user, error } = await supabase.from('User').select('*').eq('user_id', userId).single();
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  // console.log(user)
+  return NextResponse.json(user);
+
+}
