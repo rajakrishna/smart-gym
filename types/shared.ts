aaabf3889d067a6d3f138436805674c34396d854
Diego Espino <actual.mensch0r@icloud.com>
2025-07-30 17:01:30 -0500

    feat: add mockData and shared types for products page

--- types/shared.ts
+++ types/shared.ts
@@ -20,3 +20,58 @@ export interface Message {
   body: string;
   sent_at: string;
 }
+
+// TODO: Add the actual constants from the actual database
+// Start of Member Related Interfaces
+export interface Member {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  age: number;
+  membershipType: string;
+  memberSince: string;
+  memberShipStatus: string;
+  image: string;
+}
+
+export interface CheckInHistory {
+  id: number;
+  date: string;
+  time: string;
+  status: 'checked-in' | 'checked-out';
+}
+
+export interface ClassTaken {
+  id: number;
+  className: string;
+  instructor: string;
+  date: string;
+  time: string;
+  status: 'completed' | 'upcoming' | 'cancelled';
+}
+
+export interface Invoice {
+  id: number;
+  invoiceNumber: string;
+  date: string;
+  amount: number;
+  description: string;
+  status: 'paid' | 'pending' | 'overdue';
+}
+
+// Cafe/Nutrition Products Interface
+export interface Product {
+  product_id: string;
+  name: string;
+  product_image: string;
+  product_description: string;
+  price: number;
+  quantity: number;
+  min_quantity: number;
+  category: 'cafe' | 'drink' | 'snack' | 'protein_bar';
+  number_sold: number;
+  restock: boolean;
+  is_active: boolean;
+  sku: string;
+}
