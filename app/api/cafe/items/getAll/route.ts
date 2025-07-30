a1777f69587c3dfeb6e1d02964600b8ba0dd3b8a
Diego Espino <actual.mensch0r@icloud.com>
2025-07-30 17:04:21 -0500

    feat: add route to get cafe items and integrate it on the frontend

---
+++ app/api/cafe/items/getAll/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+
+import { createClient } from '@/utils/supabase/server';
+
+export async function GET() {
+  try {
+    const supabase = await createClient();
+
+    const { data: products, error } = await supabase.from('nutrition_products').select('*');
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    console.log(products, 'products');
+
+    return NextResponse.json(products);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
