d95d544496adb499e6f2ba62ebd7a5c63645623b
Diego Espino <actual.mensch0r@icloud.com>
2025-08-01 16:01:41 -0500

    feat: add member/cafe page and cafe constants

--- app/member/cafe/page.tsx
+++ app/member/cafe/page.tsx
@@ -1,9 +1,111 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+import React, { useEffect, useState } from 'react'
+import { Tabs, TabsTrigger, TabsList, TabsContent } from '@/components/ui/tabs'
+import { Loader2 } from 'lucide-react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import Image from 'next/image'
+import Link from 'next/link'
+import { mockProducts } from '@/constants/mockData';
+import { Product } from '@/types/shared';
+import { CATEGORIES, DEFAULT_CATEGORY, CATEGORY_LABELS } from '@/constants/cafeConstants';
+
+const MenuItemCard = ({ product }: { product: Product }) => {
     return (
-        <div>page</div>
+        <Card>
+            <Link href={`/member/cafe/${product.product_id}`} className='cursor-pointer'>
+                <CardContent className="p-0">
+                    <Image
+                        src={product.product_image}
+                        alt={product.name}
+                        width={100}
+                        height={100}
+                        className='w-full h-48 object-cover rounded-t-lg'
+                    />
+                </CardContent>
+                <CardHeader>
+                    <CardTitle>{product.name}</CardTitle>
+                    <CardDescription>{product.product_description}</CardDescription>
+                    <CardDescription className="text-lg font-semibold">${product.price.toFixed(2)}</CardDescription>
+                </CardHeader>
+            </Link>
+        </Card>
     )
 }
 
-export default page
+const CafePage = () => {
+    const [products, setProducts] = useState<Product[]>(mockProducts)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+    const [activeFilter, setActiveFilter] = useState<string>('all')
+
+    const filteredProducts = products.filter(product => {
+        if (activeFilter === DEFAULT_CATEGORY) return true
+        return product.category === activeFilter
+    })
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                setLoading(true)
+                const response = await fetch('/api/cafe/items')
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch products')
+                }
+
+                const data = await response.json()
+
+                console.log(data, 'data')
+
+                setProducts(data)
+            } catch (error) {
+                console.error('Error fetching products:', error)
+                setError('Failed to fetch products')
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        getProducts()
+    }, [])
+
+    return (
+        <div className="container mx-auto px-4 pb-10">
+            {/* Cafe Menu Title */}
+            <div className="flex justify-between items-center gap-4">
+                <h1 className="flex justify-center mx-auto text-2xl font-bold text-center">Cafe Menu</h1>
+            </div>
+
+            {/* Cafe Badges Filters */}
+            <div className="flex justify-center items-center gap-4 mt-4 mb-4">
+                <Tabs value={activeFilter} onValueChange={(value) => setActiveFilter(value as string)}>
+                    <TabsList className="mx-auto">
+                        <TabsTrigger className="cursor-pointer text-lg px-6 py-3" value={DEFAULT_CATEGORY}>All</TabsTrigger>
+                        {CATEGORIES.map((category) => (
+                            <TabsTrigger key={category} className="cursor-pointer text-lg px-6 py-3" value={category}>{CATEGORY_LABELS[category]}</TabsTrigger>
+                        ))}
+                    </TabsList>
+                    <TabsContent className='grid grid-cols-2 lg:grid-cols-3 gap-4 mt-4' value={activeFilter}>
+                        {loading ? (
+                            <div className="flex justify-center items-center h-full">
+                                <Loader2 className="w-8 h-8 animate-spin" />
+                            </div>
+                        ) : error ? (
+                            <div className="flex justify-center items-center h-full">
+                                <p className="text-red-500">{error}</p>
+                            </div>
+                        ) : filteredProducts.map((product) => (
+                            <MenuItemCard
+                                key={product.product_id}
+                                product={product}
+                            />
+                        ))}
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </div>
+    )
+}
+
+export default CafePage
