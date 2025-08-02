2ed158dd0384c5cbabc60706b04bf937c150a86a
Joe Aguado <aguado.joe@gmail.com>
2025-08-02 13:35:38 -0500

    feat: create formatted API for member dashboard (GYM-86)

--- app/api/classes/memberdash/route.ts
+++ app/api/classes/memberdash/route.ts
@@ -1,13 +1,27 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import { createClient } from '@/utils/supabase/server';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   const supabase = await createClient();
-  const allowedCategories = ['yoga', 'cycling', 'boxing', 'aquatic', 'hiit'];
-
+  const { searchParams } = new URL(req.url);
+  const date = searchParams.get('date');
+  const allowedCategories = [
+    'yoga',
+    'Yoga',
+    'cycling',
+    'Cycling',
+    'boxing',
+    'Boxing',
+    'aquatic',
+    'Aquatic',
+    'hiit',
+    'HIIT',
+    'Hitt',
+  ];
+  console.log('Received date query param', date);
   try {
-    const { data, error } = await supabase
+    let query = supabase
       .from('classes')
       .select(
         `
@@ -26,8 +40,15 @@ export async function GET() {
                 )
       `
       )
-      .in('category', allowedCategories)
-      .order('time', { ascending: true });
+      .in('category', allowedCategories);
+
+    if (date) {
+      query = query.eq('scheduled_on', date);
+    }
+
+    query = query.order('time', { ascending: true });
+
+    const { data, error } = await query;
 
     if (error) {
       console.error('DB Error:', error.message);
@@ -44,8 +65,15 @@ export async function GET() {
 
     const transformed = data.map(c => {
       const coach = Array.isArray(c.coach) ? c.coach[0] : c.coach;
-      function formatISOTime(dateTimeString: string) {
-        const date = new Date(dateTimeString);
+      const categoryKey = c.category.toLowerCase();
+      function formatTimeString(timeString: string): string {
+        const [hours, minutes] = timeString.split(':').map(Number);
+
+        const date = new Date();
+        date.setHours(hours);
+        date.setMinutes(minutes);
+        date.setSeconds(0);
+
         return date.toLocaleTimeString('en-US', {
           hour: 'numeric',
           minute: '2-digit',
@@ -57,9 +85,9 @@ export async function GET() {
         class_id: c.class_id,
         category: c.category,
         coach_name: coach ? `${coach.first_name}` : 'Coach TBD',
-        time: formatISOTime(c.time),
+        time: formatTimeString(c.time),
         capacity: c.capacity,
-        src: categoryImageMap[c.category] || '/assets/gc3.png',
+        src: categoryImageMap[categoryKey] || '/assets/gc3.png',
       };
     });
 
