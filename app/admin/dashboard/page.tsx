d3fb835bfeac1cceca49396b96d6a1df63441f14
Diego Espino <actual.mensch0r@icloud.com>
2025-08-01 21:45:25 -0500

    feat: add shadcn dashboard section cards and chart-area-interactive on admin dashboard (GYM-100)

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,10 +1,12 @@
-import UnderConstruction from '@/components/layouts/shared/UnderConstruction'
+import DashboardPageStatCards from '@/components/admin/dashboard/DashboardPageStatCards'
+import { SectionCards } from '@/components/layouts/admin/section-cards'
 import React from 'react'
 
 const page = () => {
     return (
-        <div>
-            <UnderConstruction />
+        <div className="container mx-auto py-10 px-4">
+            <SectionCards />
+            <DashboardPageStatCards />
         </div>
     )
 }
