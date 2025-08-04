0e990aeddfba75f261440c1bb57da9221d5ab667
Diego Espino <actual.mensch0r@icloud.com>
2025-08-04 13:24:05 -0500

    feat: add proper error printing and try catch block to /api/cafe/items/[productId]

--- app/api/cafe/items/[productId]/route.ts
+++ app/api/cafe/items/[productId]/route.ts
@@ -3,19 +3,31 @@ import { NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 
 export async function GET(request: Request, { params }: { params: Promise<{ productId: string }> }) {
-  const { productId } = await params;
+  try {
+    const { productId } = await params;
 
-  const supabase = await createClient();
+    if (!productId) {
+      return NextResponse.json({ error: 'Product ID is required' }, { status: 400 });
+    }
 
-  const { data: product, error } = await supabase
-    .from('nutrition_products')
-    .select('*')
-    .eq('product_id', productId)
-    .single();
+    const supabase = await createClient();
 
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    const { data: product, error } = await supabase
+      .from('nutrition_products')
+      .select('*')
+      .eq('product_id', productId)
+      .single();
+
+    console.log('product', product);
+
+    if (error) {
+      console.error('Error fetching product:', error);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.error('Error fetching product:', error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
-
-  return NextResponse.json(product);
 }
