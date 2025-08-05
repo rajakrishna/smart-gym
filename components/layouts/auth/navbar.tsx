8d43a5bf400f05618b926627dd139ea30cd534a2
Joe Aguado <aguado.joe@gmail.com>
2025-08-04 20:32:05 -0500

    chore: remove login form and Admin button at the top of page (GYM-79)

--- components/layouts/auth/navbar.tsx
+++ components/layouts/auth/navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
 import LABELS from '@/constants/labels';
 
 export function AuthNavbar() {
@@ -14,12 +13,12 @@ export function AuthNavbar() {
             </Link>
 
             {/* Right hand side of navbar */}
-            <Button asChild className='w-[100px] text-black' >
-                {/* TODO: Change this to scroll to the login section on the landing page */}
+            {/* <Button asChild className='w-[100px] text-black' >
+
                 <Link href="admin/dashboard">
                     {LABELS.nav.auth.login}
                 </Link>
-            </Button>
+            </Button> */}
 
         </nav>
     );
