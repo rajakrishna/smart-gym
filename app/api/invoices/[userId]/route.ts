3da269fc00ee019d33d963d79d4f5a61dc9e5275
gojoego <josephjgallego@gmail.com>
2025-08-06 16:09:21 -0500

    adding try/catch to get invoices by user_id endpoint

--- app/api/invoices/[userId]/route.ts
+++ app/api/invoices/[userId]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 import { invoiceIdSchema } from '@/lib/zod/invoiceSchema';
 import { treeifyError } from 'zod';
@@ -8,33 +8,38 @@ export async function GET(
   req: NextRequest,
   context: { params: { userId: string } }
 ) {
+    try {
+        const { userId } = await Promise.resolve(context.params);
+        const parsed = invoiceIdSchema.safeParse({ user_id: userId }); 
+        if (!parsed.success) {
+            const structuredError = treeifyError(parsed.error);
+            return NextResponse.json(
+            {
+                error: 'invalid user id',
+                details: structuredError,
+            },
+            { status: 400 }
+            );
+        }     
+        const { user_id } = parsed.data;
 
-  const { userId } = await Promise.resolve(context.params);
+        const supabase = await createClient();
+        const { data: invoices, error } = await supabase
+            .from('user_invoice')
+            .select('*')
+            .eq('user_id', user_id);
+        if (error) {
+            return NextResponse.json({ error: error.message }, { status: 500 });
+        }
 
-  const parsed = invoiceIdSchema.safeParse({ user_id: userId });
-
-  if (!parsed.success) {
-    const structuredError = treeifyError(parsed.error);
-    return NextResponse.json(
-      {
-        error: 'invalid user id',
-        details: structuredError,
-      },
-      { status: 400 }
-    );
-  }
-
-  const { user_id } = parsed.data;
-
-  const supabase = await createClient();
-  const { data: invoices, error } = await supabase
-    .from('user_invoice')
-    .select('*')
-    .eq('user_id', user_id);
-
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-
-  return NextResponse.json(invoices);
+        return NextResponse.json(invoices);
+    } catch (error) {
+        return NextResponse.json(
+            {
+                error: 'unexpected server error',
+                details: error instanceof Error ? error.message : String(error)
+            },
+            { status: 500}
+        );
+    }
 }
